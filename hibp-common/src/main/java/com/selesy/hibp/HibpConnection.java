c1fcadb375c2b98a3a31b717cc54b45365a8b5ea
Steve Moyer <smoyer1@selesy.com>
2018-04-05 20:51:07 -0400

    Makes the HibpConnectionFactory a subinterface of HibpConnection

--- hibp-common/src/main/java/com/selesy/hibp/HibpConnection.java
+++ hibp-common/src/main/java/com/selesy/hibp/HibpConnection.java
@@ -12,6 +12,29 @@ import java.util.stream.Stream;
 public interface HibpConnection extends AutoCloseable {
 
     /**
+     * Provides the API for a generic connection factory that specific clients
+     * can implement.  This allows the HibpClientBase to be implementation
+     * agnotistic while retaining the ability to create as many connections
+     * as it needs during the course of its work.
+     */
+    @FunctionalInterface
+    public interface Factory {
+
+        /**
+         * Creates an HIBP compatible connection (in part defined by the values
+         * in the configuration file), connects to the remote server and allows
+         * the caller to access a Stream containing the lines of the text/plain
+         * response entity body.
+         * 
+         * @returns A Possible object containing either aconfigured and connected
+         *          HibpConnection or a Throwble describing the failure thrown
+         *          while trying to create the connection.
+         */
+        Possible<HibpConnection> connect(HibpConfiguration configuration, String hashPrefix);
+
+    }
+
+    /**
      * Supplies a stream of String objects, each representing one line
      * returned from an HIBP server.  The contents of that line are expected
      * to contain a hashSuffix and a count separated by a colon which can
